852249ba61b433607454d8e44c4d16e6b8cce30b
ItsGabeTheBabe <certifiedbeautygabe@gmail.com>
2019-03-12 22:22:06 -0700

    added the feature to be able to do /signup, /profile, and added a sign up form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router} from 'react-router-dom';
+import Route from 'react-router-dom/Route';
 
 class App extends Component {
   render() {
     return (
+      <Router>
       <div >
-      
+      <Route path="/" exact strict render={
+        ()=>{
+        return ( <h1>Welcome Home</h1>);
+      }
+      }/>
+
+      <Route path="/signup" exact strict render={
+        ()=>{
+        return (
+          <div>
+            <h1>Sign Up Biiitch</h1>
+            <legend>New to LFG? Sign up!</legend>
+            <form accept-charset="UTF-8" action="" method="post">
+                <input class="signupbox" name="name" placeholder="Full Name" type="text"/>
+                  <br/>
+                <input class="signupbox" name="username" placeholder="Username" type="text"/>
+                  <br/>
+                <input class="signupbox" name="password" placeholder="Password" type="password"/>
+                  <br/>
+                <button class="btn btn-warning" type="submit">Sign up for WebApp</button>
+            </form>
+          </div>
+        );
+      }
+      }/>
+
+      <Route path="/profile" exact strict render={
+        ()=>{
+        return ( <h1>Your Profile</h1>);
+      }
+      }/>
       </div>
+      </Router>
     );
   }
 }
